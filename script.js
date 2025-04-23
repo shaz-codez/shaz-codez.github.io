5c7b2058657f503633788417dfc6d1bf34274423
Shaz <muhammedshazpp223@gmail.com>
2025-04-23 20:34:27 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function showDetails(car) {
+  if (car === 'landcruiser') {
+    alert("The Toyota Land Cruiser is built for both luxury and rugged off-roading. With a 5.7L V8 engine, it delivers top-tier performance and reliability.");
+  } else if (car === 'bmwx7') {
+    alert("The BMW X7 is a premium luxury SUV with high-end features, sleek design, and smooth driving experience, powered by a 3.0L turbo engine.");
+  }
+}
+
